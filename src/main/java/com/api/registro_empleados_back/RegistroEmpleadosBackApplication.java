0834524de7399feed86639b3a5231e35de7083f3
lvacosta <53828175+laurav09@users.noreply.github.com>
2022-12-13 15:04:57 -0500

    proyecto base

---
+++ src/main/java/com/api/registro_empleados_back/RegistroEmpleadosBackApplication.java
@@ -0,0 +1,13 @@
+package com.api.registro_empleados_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroEmpleadosBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroEmpleadosBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/registro_empleados_back/RegistroEmpleadosBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.registro_empleados_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroEmpleadosBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
